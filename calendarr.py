2a309269465110eaf56883b7d70d3558065e33aa
SutirthaChattopadhyay <77205993+SutirthaChattopadhyay@users.noreply.github.com>
2022-03-23 10:28:10 +0530

    Add files via upload

---
+++ calendarr.py
@@ -0,0 +1,7 @@
+#python program to print specific month of a selected year
+
+import calendar
+year=int(input("Enter Year : "))
+month=int(input("Enter Month : "))
+print("\n",calendar.month(year,month))
+print("\n ©Sutirtha Chattopadhyay 14-02-2022")
